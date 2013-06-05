119172d59a8dc82a3a584336c745ef16c53db212
Shivaram U <shivaram.u@quadstor.com>
2013-06-05 23:17:55 +0530

    Release 2.2.2

--- build/fixversion.sh
+++ build/fixversion.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x 
 for i in `ls -1`; do
-	sed -i -e "s/2.2.1/2.2.1/g" $i
-	sed -i -e "s/2.2.1-x86_64/2.2.1-x86_64/g" $i
+	sed -i -e "s/2.2.1/2.2.2/g" $i
+	sed -i -e "s/2.2.1-x86_64/2.2.2-x86_64/g" $i
 done
