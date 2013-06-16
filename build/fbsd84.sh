94163281bbc9fd2febe35823352b3e218da39994
Shivaram U <shivaram.u@quadstor.com>
2013-06-16 12:27:20 +0530

    Add FreeBSD 8.4 build support

---
+++ build/fbsd84.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x
+curdir=`pwd`
+sh buildinit.sh bsd
+cd /quadstor/quadstor/pgsql && gmake install
+cd $curdir
+sed -i -e "s/FreeBSD8.2/FreeBSD8.4/" createpkg.sh
+sed -i -e "s/FreeBSD8.2/FreeBSD8.4/" createitf.sh
+sed -i -e "s/FreeBSD 8.2/FreeBSD 8.4/" pkg-post-itf.sh
+sed -i -e "s/FreeBSD 8.2/FreeBSD 8.4/" pkg-post.sh
+sh createpkg.sh && sh createitf.sh
