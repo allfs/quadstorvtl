3f783af2b81a2bc2958207dedc50ca711e7cf8c6
Shivaram U <shivaram.u@quadstor.com>
2013-10-05 23:22:56 +0530

    No need to add x86 suffix to Release

--- build/rhel6x86.sh
+++ build/rhel6x86.sh
@@ -3,8 +3,8 @@ set -x
 sh buildinit.sh rhel6x86
 cp -f quadstorcore.spec quadstorcorerhel6x86.spec
 cp -f quadstoritf.spec quadstoritfrhel6x86.spec
-sed -i -e "s/Release: .*/Release: rhel6x86/g" quadstorcorerhel6x86.spec
-sed -i -e "s/Release: .*/Release: rhel6x86/g" quadstoritfrhel6x86.spec
+sed -i -e "s/Release: .*/Release: rhel6/g" quadstorcorerhel6x86.spec
+sed -i -e "s/Release: .*/Release: rhel6/g" quadstoritfrhel6x86.spec
 sed -i -e "s/CentOS 5/CentOS 6 x86/g" quadstorcorerhel6x86.spec
 sed -i -e "s/CentOS 5/CentOS 6 x86/g" quadstoritfrhel6x86.spec
 sed -i -e "s/sh build.sh$/sh build.sh x86/g" quadstorcorerhel6x86.spec
